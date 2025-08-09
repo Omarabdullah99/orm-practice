427559517c2f87af84e3ca3fdcede0f0aa718c92
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-08-09 16:32:38 +0600

    create-update-delte

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/create', [UserController::class, 'createUser']);
+Route::get('/update', [UserController::class, 'updateUser']);
+Route::get('/delete', [UserController::class, 'deleteUser']);
